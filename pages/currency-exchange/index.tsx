678680676a44c40b56d77fa9554c502de51791f1
haroon.javed <haroon.javed@tintash.com>
2022-04-11 09:51:17 +0500

    api with currency connected

--- pages/currency-exchange/index.tsx
+++ pages/currency-exchange/index.tsx
@@ -2,12 +2,14 @@ import { Button, Grid, Input } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
-import * as yup from 'yup';
+import * as yup from "yup";
+import { useLoaded } from "../../components/customHooks/loading";
 
 let schema = yup.object().shape({
-    currency: yup.number().required().positive().integer(),
-  });
+  currency: yup.number().required().positive().integer(),
+});
 export default function CurrencyExchange({
   currencies,
 }: {
@@ -16,38 +18,58 @@ export default function CurrencyExchange({
   const [state, setState] = useState<{
     from: null | string;
     to: null | string;
-    value:string |null;
+    value: string | null;
+    loading: boolean;
+    rate: string;
   }>({
     from: null,
     to: null,
-    value:""
+    value: "",
+    loading: false,
+    rate: "",
   });
+  const loaded = useLoaded();
+  const getCalculatedAmout = () => {
+    setState({ ...state, loading: true });
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+        "X-RapidAPI-Key": "4ad079a7f8msh6b17288311b9373p152cddjsnd94615b93e44",
+      },
+    };
 
-  const getCalculatedAmout=()=>{
+    fetch(
+      `https://currency-exchange.p.rapidapi.com/exchange?to=${state.to}&from=${state.from}&q=1.0`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setState({ ...state, loading: false, rate:response });
+      })
+      .catch((err) => {
+        console.error(err);
+        setState({ ...state, loading: false });
+      });
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let input = e.target.value;
 
-  }
-  const onChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
-    let input = e.target.value ;
- 
-    if(input.length === 0){
-        setState({...state,value:""})
-        return;
+    if (input.length === 0) {
+      setState({ ...state, value: "" });
+      return;
     }
-    try{
-        schema
+    try {
+      schema
         .isValid({
-            currency: parseFloat(input),
+          currency: parseFloat(input),
         })
         .then(function (valid) {
-          valid && setState({...state,value:input.toString()})
+          valid && setState({ ...state, value: input.toString() });
         });
-    }catch(e){
-
-    }
-  
-     
-
-  }
+    } catch (e) {}
+  };
 
   return (
     <>
@@ -56,52 +78,76 @@ export default function CurrencyExchange({
           <h2>Exchange Money</h2>
         </div>
         <Grid container justifyContent="center">
-        <Grid item  xs={8}>
-          <Grid container spacing={2} >
-            <Grid item xs>
-              <Autocomplete
-                options={currencies}
-                id="controlled-demo"
-                value={state.from}
-                onChange={(event: any, newValue: string | null) => {
-                  setState({ ...state, from: newValue });
-                }}
-                renderInput={(params) => (
-                  <TextField {...params} label="FROM" variant="standard" />
-                )}
-              />
-            </Grid>
-            <Grid item xs>
-              <Autocomplete
-                options={currencies}
-                id="controlled-demo"
-                value={state.to}
-                onChange={(event: any, newValue: string | null) => {
-                  setState({ ...state, to: newValue });
-                }}
-                renderInput={(params) => (
-                  <TextField {...params} label="To" variant="standard" />
-                )}
-              />
-            </Grid>
-
-            <Grid container spacing={2} style={{ marginTop: 50,paddingLeft:16 }}>
-              <Grid item xs={6}>
-                <Grid>
-                  <Input fullWidth inputProps={{type: "number", pattern: "[0-9]+"}} onChange={onChange}  placeholder="0" value={state.value? parseFloat(state.value ):0} style={{ height: 80, fontSize: 30 }} />
-                </Grid>
-                <Grid><Button fullWidth variant="contained">Calculate</Button></Grid>
+          <Grid item xs={8}>
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <Autocomplete
+                  options={currencies}
+                  id="controlled-demo"
+                  value={state.from}
+                  onChange={(event: any, newValue: string | null) => {
+                    setState({ ...state, from: newValue });
+                  }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="FROM" variant="standard" />
+                  )}
+                />
+              </Grid>
+              <Grid item xs>
+                <Autocomplete
+                  options={currencies}
+                  id="controlled-demo"
+                  value={state.to}
+                  onChange={(event: any, newValue: string | null) => {
+                    setState({ ...state, to: newValue });
+                  }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="To" variant="standard" />
+                  )}
+                />
               </Grid>
 
-              <Grid item  xs>
-              <Grid>
-                 <span style={{fontSize:30}}>
-                     100
-                 </span>
+              <Grid
+                container
+                spacing={2}
+                style={{ marginTop: 50, paddingLeft: 16 }}
+              >
+                <Grid item xs={6}>
+                  <Grid>
+                    <Input
+                      fullWidth
+                      inputProps={{ type: "number", pattern: "[0-9]+" }}
+                      onChange={onChange}
+                      placeholder="0"
+                      value={state.value ? parseFloat(state.value) : 0}
+                      style={{ height: 80, fontSize: 30 }}
+                    />
+                  </Grid>
+                  <Grid>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      onClick={() => {
+                        getCalculatedAmout();
+                      }}
+                    >
+                      {" "}
+                      {state.loading ? (
+                        <CircularProgress style={{ color: "white" }} />
+                      ) : (
+                        "Calculate"
+                      )}
+                    </Button>
+                  </Grid>
+                </Grid>
+
+                <Grid item xs>
+                  <Grid>
+                    <span style={{ fontSize: 30 }}>{loaded && state.rate && eval(`${state.rate}*${state.value}`)}</span>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
           </Grid>
         </Grid>
       </Box>
--- pages/translation/index.tsx
+++ pages/translation/index.tsx
@@ -3,64 +3,65 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { useEffect, useState } from "react";
-import * as yup from 'yup';
+import CircularProgress from "@mui/material/CircularProgress";
+import { useLoaded } from "../../components/customHooks/loading";
 
-
-export default function Translation({
-  languages,
-}: {
-    languages: string[];
-}) {
+export default function Translation({ languages }: { languages: string[] }) {
   const [state, setState] = useState<{
-
-    value:string |null;
-    translatedInput:string;
-    translated:string; 
+    value: string | null;
+    translationLanguage: string;
+    translated: string;
+    loading: boolean;
   }>({
-  
-    value:"",
-    translatedInput: "",
-    translated: ""
+    value: "",
+    translationLanguage: "es",
+    translated: "",
+    loading: false,
   });
-
-
-  const onChange = (input:string|null,key:string) => {
-
- 
-    if(input){
-        setState({...state,[key]:input})
-        return;
+  // to remove warning of null in conditional rendering
+  const loaded = useLoaded();
+  const onChange = (input: string | null, key: string) => {
+    if (key) {
+      setState({ ...state, [key]: input });
+      return;
     }
-    
-  
-     
+  };
 
-  }
-
-  const onTranslate =async ()=>{
+  const onTranslate = async () => {
     const encodedParams = new URLSearchParams();
-encodedParams.append("target", "es");
-encodedParams.append("q", state.value!);
+    encodedParams.append("target", state.translationLanguage);
+    encodedParams.append("q", state.value!);
 
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-		'X-RapidAPI-Key': '4ad079a7f8msh6b17288311b9373p152cddjsnd94615b93e44'
-	},
-	body: encodedParams
-};
-
-const res = await fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-const result: TranslationData = await res.json();
-const Translated = result.data.translations.map(item=>item.translatedText)
-console.log(Translated);
-setState({...state, translated:Translated[0]??""})
-
-  }
-console.log(state);
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+        "X-RapidAPI-Key": "4ad079a7f8msh6b17288311b9373p152cddjsnd94615b93e44",
+      },
+      body: encodedParams,
+    };
+    setState({ ...state, loading: true });
+    const res = await fetch(
+      "https://google-translate1.p.rapidapi.com/language/translate/v2",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const result: TranslationData = response;
+        const Translated = result.data.translations.map(
+          (item) => item.translatedText
+        );
+        console.log(Translated);
+        setState({ ...state, translated: Translated[0] ?? "", loading: false });
+      })
+      .catch((err) => {
+        console.error(err);
+        setState({ ...state, loading: false });
+      });
+  };
+  console.log(state);
   return (
     <>
       <Box>
@@ -68,42 +69,85 @@ console.log(state);
           <h2>Language Translation</h2>
         </div>
         <Grid container justifyContent="center">
-        <Grid item  xs={8}>
-          <Grid container spacing={2} >
-            <Grid item xs>
-              <Autocomplete
-                options={languages}
-
-                value={state.translatedInput}
-                onChange={(e: React.SyntheticEvent<Element, Event>, input:string|null)=>{onChange(input, "translatedInput")}}
-                renderInput={(params) => (
-                  <TextField {...params} name="translated" label="Select Language to translate " variant="standard" />
-                )}
-              />
-            </Grid>
-          
-
-            <Grid container spacing={2} style={{ marginTop: 50,paddingLeft:16 }}>
-              <Grid item xs={6}>
-                <Grid>
-                  <Input fullWidth onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{onChange(e.target.value,"value")}} inputProps={{type: "text", maxLength:"500"}} 
-                //   onChange={onChange}
-                   placeholder="Enter your Text here"   
-                //    value={state.value}
-                    style={{ height: 80, fontSize: 30 }} />
-                </Grid>
-                <Grid><Button fullWidth variant="contained" onClick={onTranslate}>Translate</Button></Grid>
+          <Grid item xs={8}>
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <Autocomplete
+                  options={languages}
+                  value={state.translationLanguage}
+                  onChange={(
+                    e: React.SyntheticEvent<Element, Event>,
+                    input: string | null
+                  ) => {
+                    onChange(input, "translationLanguage");
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      name="translated"
+                      label="Select Language to translate "
+                      variant="standard"
+                      
+                    />
+                  )}
+                />
               </Grid>
 
-              <Grid item  xs>
-              <Grid>
-                 <span style={{fontSize:30}}>
-                     Translated text
-                 </span>
+              <Grid
+                container
+                spacing={3}
+                justifyContent="center"
+                style={{ marginTop: 50, paddingLeft: 16 }}
+              >
+                <Grid item xs={12} md={5} sx={{ paddingRight: 16 }}>
+                  <Grid>
+                    <Input
+                      fullWidth
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        onChange(e.target.value, "value");
+                      }}
+                      inputProps={{ type: "text", maxLength: "500" }}
+                      //   onChange={onChange}
+                      placeholder="Enter your Text here"
+                      //    value={state.value}
+                      style={{ height: 80, fontSize: 30 }}
+                    />
+                  </Grid>
+                  <Grid>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      disabled={loaded && !state.value}
+                      onClick={onTranslate}
+                    >
+                      {state.loading ? (
+                        <CircularProgress style={{ color: "white" }} />
+                      ) : (
+                        "Translate"
+                      )}
+                    </Button>
+                  </Grid>
+                </Grid>
+
+                <Grid
+                  item
+                  xs={12}
+                  md={5}
+                  sx={{
+                    border: 2,
+                    borderColor: "grey",
+                    margin: 1,
+                    minHeight: 150,
+                    maxHeight: 200,
+                    overflow: "scroll",
+                  }}
+                >
+                  <Grid>
+                    <span style={{ fontSize: 30 }}>{state.translated}</span>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
           </Grid>
         </Grid>
       </Box>
@@ -112,44 +156,46 @@ console.log(state);
 }
 
 export const getStaticProps = async () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'Accept-Encoding': 'application/gzip',
-            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-            'X-RapidAPI-Key': '4ad079a7f8msh6b17288311b9373p152cddjsnd94615b93e44'
-        }
-    };
+  const options = {
+    method: "GET",
+    headers: {
+      "Accept-Encoding": "application/gzip",
+      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      "X-RapidAPI-Key": "4ad079a7f8msh6b17288311b9373p152cddjsnd94615b93e44",
+    },
+  };
 
-  const res = await fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/languages', options)
+  const res = await fetch(
+    "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+    options
+  );
   const result: LanguageData = await res.json();
 
-  const languages = result.data.languages.map(item=>item.language)
+  const languages = result.data.languages.map((item) => item.language);
   console.log(languages);
   return { props: { languages } };
 };
 
-type LanguageType={ 
-    language:string
+type LanguageType = {
+  language: string;
+};
+
+type LanguagesList = {
+  languages: LanguageType[];
+};
+
+interface LanguageData {
+  data: LanguagesList;
 }
 
-type LanguagesList={
-    languages:LanguageType[]
-}
+type TranslatedType = {
+  translatedText: string;
+};
 
-interface LanguageData{
-    data:LanguagesList
-}
+type TranslatedList = {
+  translations: TranslatedType[];
+};
 
-
-type TranslatedType={ 
-  translatedText:string
-}
-
-type TranslatedList={
-  translations:TranslatedType[]
-}
-
-interface TranslationData{
-  data:TranslatedList
+interface TranslationData {
+  data: TranslatedList;
 }
